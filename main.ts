dd2c16770eb3458155b63e2a7f1b8485bfc5cb6c
MsPhipp <77368383+MsPhipp@users.noreply.github.com>
2021-01-12 17:52:03 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030207070e0e0e0e0e0e0e03030303030303070e0e0e0e0e0e0e0e0707070707030307070e0e0e0e0e0e0e0c0c0707070303070707070707070e0e0c0c0707070303030303030303040d0d0c0c0c0c070303040404040403040d0d0c0c0c0c070303050606060403040d0d0c0c0c0c070301040404040403040d0d0c0c0c0c070303040403030303040d0d0a0a0a0a0a0808040403040404040d0d0a0f0a0a0a0808040403040a0a0a0a0a0a0a0a0a0a0808040404040a0a0a0a0a0b0b0b0b0b0b0b0b0b0b0b0b0b0a0a0a0b0b0b0909090b0b0b0b0b0b0b0a0a0a090909090909090909090b0b0b0a0a0a090909090909090909090b0b0b0a0a0a`, img`
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock1, function (sprite, location) {
+    mySprite.say("Ohh! a rock!", 200)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    mySprite.startEffect(effects.fire, 500)
+    mySprite.say("Owwwwww", 500)
+    info.changeLifeBy(-1)
+    mySprite.y += 2
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock2, function (sprite, location) {
+    tiles.setTileAt(location, sprites.castle.tileGrass3)
+    tiles.setTileAt(tiles.getTileLocation(tiles.locationXY(location, tiles.XY.column) + 1, tiles.locationXY(location, tiles.XY.row) - 0), sprites.castle.tilePath5)
+})
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030207070e0e0e0e0e0e0e03030303030303070e0e0e0e0e0e0e0e0707070707030307070e0e0e0e0e0e0e0c0c0707070303070707070707070e0e0c0c0707070303030303030303040d0d0c0c0c0c070303040404040403040d0d0c0c0c0c070303050606060403040d0d0c0c0c0c070301040404040403040d0d0c0c0c0c070303040403030303040d0d0a0a0a0a0f0808040403040404040d0d0a0a0a0a0f0808040403040a0a0a0a0a0a0a0a0a0f0808040404040a0a0a0a0a0b0b0b0b0b0b0b0b0b0b0b0b0b0a0a0a0b0b0b0909090b0b0b0b0b0b0b0a0a0a090909090909090909090b0b0b0a0a0a090909090909090909090b0b0b0a0a0a`, img`
     . . . . . . . 2 . . . . . . . . 
     . . . . . . . 2 . . . . . . . . 
     2 2 2 2 2 . . 2 . . . . . . . . 
@@ -15,8 +29,9 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030207070e0e0e0e0e0e0
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, [myTiles.transparency16,sprites.dungeon.chestClosed,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.floorLight0,sprites.dungeon.stairWest,sprites.dungeon.darkGroundNorthWest1,sprites.castle.rock0,sprites.dungeon.hazardLava0,sprites.dungeon.hazardWater,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.builtin.forestTiles0,sprites.builtin.forestTiles21,sprites.castle.shrub,sprites.castle.rock1], TileScale.Sixteen))
-let mySprite = sprites.create(img`
+    `, [myTiles.transparency16,sprites.dungeon.chestClosed,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.floorLight0,sprites.dungeon.stairWest,sprites.dungeon.darkGroundNorthWest1,sprites.castle.rock0,sprites.dungeon.hazardLava0,sprites.dungeon.hazardWater,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.builtin.forestTiles0,sprites.builtin.forestTiles21,sprites.castle.shrub,sprites.castle.rock2], TileScale.Sixteen))
+info.setLife(5)
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . 4 4 4 4 4 4 4 . . . . . 
